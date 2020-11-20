ae382614cbbaca11c25851c5b0163dd2b52ea5d6
Shamim Hussain <snirjhar@gmail.com>
2020-11-19 19:49:49 -0500

    modified run script

--- run.sh
+++ run.sh
@@ -1,15 +1,33 @@
 num_worker=$1
-beta=10.0
-S=2
-tau=1
-steps=50
-device=cpu:0
+beta=${2:-1.0}
+S=${3:-$1}
+tau=${4:-1}
+steps=${5:-50}
+device=${6:-cpu:0}
 
+
+echo ----------------------------------
+
+echo num_worker = $num_worker
+echo beta = $beta
+echo S = $S 
+echo tau = $tau
+echo steps = $steps
+echo device = $device
+
+echo ----------------------------------
+
+echo Generating knownhosts.json
 python knownhosts_gen.py $num_worker
 
+echo Starting MASTER
 python main.py $num_worker --beta $beta --S $S --tau $tau --steps $steps --device $device &
 
 for w_i in $(seq 0 $(expr $num_worker - 1))
 do
+    echo Starting WORKER:$w_i
     python main.py $w_i --beta $beta --S $S --tau $tau --steps $steps --device $device &
 done
+
+jobs
+
