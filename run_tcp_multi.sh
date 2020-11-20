6055c12643ad13e1aaaf5c44cecb35619c44a9cb
Shamim Hussain <snirjhar@gmail.com>
2020-11-20 08:24:47 -0500

    added tcp multi

--- run_tcp_multi.sh
+++ run_tcp_multi.sh
@@ -5,7 +5,7 @@ beta=${4:-1.0}
 S=${5:-$2}
 tau=${6:-1}
 steps=${7:-50}
-device=${8:-cpu:0}
+num_device=${8:-6}
 
 
 echo ----------------------------------
@@ -15,18 +15,17 @@ echo beta = $beta
 echo S = $S 
 echo tau = $tau
 echo steps = $steps
-echo device = $device
 
 echo ----------------------------------
 
 echo Starting MASTER
-python $script $num_worker $num_worker $port --beta $beta --S $S --tau $tau --steps $steps --device $device &
+python $script $num_worker $num_worker $port --beta $beta --S $S --tau $tau --steps $steps --device cuda:0 &
 sleep 2
 
 for w_i in $(seq 0 $(expr $num_worker - 1))
 do
     echo Starting WORKER:$w_i
-    python $script $num_worker $w_i $port --beta $beta --S $S --tau $tau --steps $steps --device $device &
+    python $script $num_worker $w_i $port --beta $beta --S $S --tau $tau --steps $steps --device cuda:$(expr w_i % $num_device) &
     sleep 0.5
 done
 
