8b8277853efc96411c83e424aef97a2bb98fa38f
Md Shamim Hussain <snirjhar@gmail.com>
2020-12-07 17:28:03 -0500

    Fixed indentation

--- plotting_tool.py
+++ plotting_tool.py
@@ -15,40 +15,40 @@ if __name__ == '__main__':
         dataframes[sf.name]=pd.read_pickle(str(sf/'logs.pkl'))
 
 
-breakname = lambda name: ' '.join(v[0].upper()+v[1:].lower()
-                                  for v in name.split('_'))
+    breakname = lambda name: ' '.join(v[0].upper()+v[1:].lower()
+                                      for v in name.split('_'))
 
 
-matplotlib.use('agg')
+    matplotlib.use('agg')
 
-units = {'wall_time':' (seconds)','global_step':''}
+    units = {'wall_time':' (seconds)','global_step':''}
 
-xlim = {'wall_time':2,'global_step':200}
-
-
-figs = {}
-figsize=[4,3]
-for k, df in dataframes.items():
-    for xl in df.columns[:2]:
-        for yl in df.columns[2:]:
-            if (xl,yl) not in figs:
-                figs[(xl,yl)] = plt.figure(figsize=figsize)
-                
-            fig = figs[(xl,yl)]
-            ax = fig.gca()
-            ax.title.set_text(f'{breakname(yl)} vs. {breakname(xl)}')
-            ax.plot(df[xl],df[yl], label=k)
-            ax.set_xlim(left=0, right=xlim[xl])
-            ax.set_xlabel(breakname(xl)+units[xl])
-            ax.set_ylabel(breakname(yl))
-            ax.legend()
-            ax.grid(True)            
+    xlim = {'wall_time':2,'global_step':200}
 
 
-figdir = folder/'Figures'
-figdir.mkdir(exist_ok=True)
-for (xl, yl), f in figs.items():
-    f.tight_layout()
-    f.savefig(figdir/f'{breakname(yl)} vs. {breakname(xl)}.png',dpi=600)
-    f.savefig(figdir/f'{breakname(yl)} vs. {breakname(xl)}.pdf')
-    #f.savefig(figdir/f'{breakname(yl)} vs. {breakname(xl)}.svg')
+    figs = {}
+    figsize=[4,3]
+    for k, df in dataframes.items():
+        for xl in df.columns[:2]:
+            for yl in df.columns[2:]:
+                if (xl,yl) not in figs:
+                    figs[(xl,yl)] = plt.figure(figsize=figsize)
+
+                fig = figs[(xl,yl)]
+                ax = fig.gca()
+                ax.title.set_text(f'{breakname(yl)} vs. {breakname(xl)}')
+                ax.plot(df[xl],df[yl], label=k)
+                ax.set_xlim(left=0, right=xlim[xl])
+                ax.set_xlabel(breakname(xl)+units[xl])
+                ax.set_ylabel(breakname(yl))
+                ax.legend()
+                ax.grid(True)            
+
+
+    figdir = folder/'Figures'
+    figdir.mkdir(exist_ok=True)
+    for (xl, yl), f in figs.items():
+        f.tight_layout()
+        f.savefig(figdir/f'{breakname(yl)} vs. {breakname(xl)}.png',dpi=600)
+        f.savefig(figdir/f'{breakname(yl)} vs. {breakname(xl)}.pdf')
+        #f.savefig(figdir/f'{breakname(yl)} vs. {breakname(xl)}.svg')
